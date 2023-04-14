a67e5a6886669ac1a8f0f3c4a6a70f91dc8ed700
Victor Alexandre Vieira de Matos <112444309+VictorAVMatos@users.noreply.github.com>
2023-04-14 17:29:53 -0300

    Add files via upload

---
+++ Preco_Viagem_Por_Distancia.js
@@ -0,0 +1,8 @@
+const PrecoCombustivel = 5.349;
+const KmPorLitros = 11;
+const DistanciaEmKm = 244;
+
+const LitrosConsumidos = DistanciaEmKm / DistanciaEmKm
+const ValorGasto = LitrosConsumidos * PrecoCombustivel
+
+console.log (ValorGasto.tofixed(2))
